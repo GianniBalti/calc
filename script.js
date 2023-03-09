d612fd5a160211370fd3ecb1bf6acd089384b52e
GianniBalti <gianni@baltisolar.be>
2023-03-09 20:54:42 +0100

    ui update

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let numberContainer = document.getElementById("numberContainer");
+console.log(numberContainer.className);
 
-let divs = document.cre
+function createNumberDivs() {
+    for (let index = 0; index < 9; index++) {
+        const numberDiv = document.createElement("div");
+        numberDiv.id = "numberCell";
+        numberDiv.className = `"number-button-${index+1}"`; 
+        numberContainer.append(numberDiv);
+        
+    }
+}
+
+function main() {
+    createNumberDivs();
+}
+
+main();
